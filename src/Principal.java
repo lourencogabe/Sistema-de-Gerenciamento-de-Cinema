7f494f07f2a925dadc824eb55cbe76d35ca38f1e
Gabriel Lourenço <gabriels211@hotmail.com>
2024-05-02 09:56:44 -0300

    feat: adiciona importações do file manager e menu principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,15 @@
 public class Principal {
     public static void main(String[] args) {
 
+        System.out.print("##-----------CINEMA ADMIN-----------##\n");
+        System.out.print("|------------------------------------|\n");
+        System.out.print("| Opção 1 - Vender Ingresso \n");
+        System.out.print("| Opção 2 - Cadastrar Filme \n");
+        System.out.print("| Opção 3 - Filme Cadastrados \n");
+        System.out.print("| Opção 4 - Agendar Sessão  \n");
+        System.out.print("| Opção 5 - Calcular Vendas \n");
+        System.out.print("|------------------------------------|\n");
+        System.out.print("| Opção: \n");
+
     }
 }
--- src/br/edu/up/models/FileManager.java
+++ src/br/edu/up/models/FileManager.java
@@ -1,4 +1,9 @@
 package br.edu.up.models;
 
+import java.io.File;
+
 public class FileManager {
+    String caminho ="/Sistema de Gerenciamento de Cinema/src/br/edu/up/arquivos";
+    File diretorio = new File(caminho + "Teste");
+    File arquivo = new File(diretorio, "ingresso.txt");
 }
